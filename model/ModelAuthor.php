5521903083a6f279314bcd90f3b8ca2c9e94b9ee
fiqriachmada <achmada1998@gmail.com>
2021-04-28 21:08:19 +0700

    UTS Basdat

---
+++ model/ModelAuthor.php
@@ -0,0 +1,38 @@
+<?php
+
+class ModelAuthor{
+    public function get($idAuthor){
+        $sql =
+        "SELECT * FROM catatan WHERE author_id=$idAuthor";
+
+        $query = koneksi()->query($sql);
+        $hasil = [];
+        while ($data = $query->fetch_assoc()) {
+            $hasil[] = $data;
+        }
+        return $hasil;
+    }
+    public function index(){
+        $idAuthor = $_SESSION['author']['id'];
+        $data = $this->get($idAuthor);
+        extract($data);
+        require_once("view/author/index.php");
+    }
+    public function prosesAddCatatan($nama, $catatan, $idAuthor){
+        $sql = "INSERT INTO catatan (nama,catatan,author_id)
+        VALUE('$nama','$catatan','$idAuthor')";
+        return koneksi()->query($sql);
+    }
+
+    public function addCatatan(){
+        $nama = $_POST['nama'];
+        $catatan = $_POST['catatan'];
+        $idAuthor = $_POST['idAuthor'];
+        if($this->prosesAddCatatan($nama,$catatan,$idAuthor)){
+            header("location: index.php?page=author&aksi=view&pesan=sukses menambah catatan");
+        }else{
+            header("location: index.php?page=author&aksi=view&pesan=gagal menambah catatan");
+        }
+    }
+
+}
---
+++ model/ModelCatatan.php
@@ -0,0 +1,22 @@
+<?php
+
+class ModelAuthor{
+    public function get(){
+        $sql =
+        "SELECT * FROM catatan";
+
+        $query = koneksi()->query($sql);
+        $hasil = [];
+        while ($data = $query->fetch_assoc()) {
+            $hasil[] = $data;
+        }
+        return $hasil;
+    }
+    public function index()
+    {
+        $data = $this->get();
+        extract($data);
+        require_once("view/catatan/index.php");
+    }
+
+}
